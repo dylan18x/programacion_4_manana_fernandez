b002ff06b8f27e5fdf9e3360662a079578b74b60
dylan18x <gadieldylan02@gmail.com>
2026-04-13 10:57:26 -0500

    CLASE-13-04-2026

---
+++ modulo_1/basics/17_ciclos_while.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Controles de Flujo Iteraciones, Ciclos repetititvos-Ciclo While")
+    println("While Basico")
+    var contador = 1
+    while(contador <= 5){
+        println(contador)
+        contador ++;
+    }
+}
