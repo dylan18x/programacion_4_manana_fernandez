1f6ef2bb3bd757366e3672bc7c53d0fc210f7370
AlexLopezF04 <j.alexander.lopez.f@gmail.com>
2026-05-27 05:57:35 -0500

    CLASE-27-05-2026

---
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/data/remote/dto/PaginateDto.kt
@@ -0,0 +1,10 @@
+package com.shopapp.data.remote.dto
+
+// data/remote/dto/PaginatedDto.kt
+
+data class PaginatedDto<T>(
+    val count:    Int,
+    val next:     String?,
+    val previous: String?,
+    val results:  List<T>,
+)
---
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package com.shopapp.di
+
+// di/RepositoryModule.kt
+
+import com.shopapp.data.repository.CategoryRepositoryImpl
+import com.shopapp.domain.repository.CategoryRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds @Singleton
+    abstract fun bindCategoryRepository(
+        impl: CategoryRepositoryImpl,
+    ): CategoryRepository
+
+    // Los demás repositorios se añaden en los módulos siguientes
+}
