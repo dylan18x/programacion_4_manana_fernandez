2053ec240b1b571d3e9da91964ae139c8255b603
AlexLopezF04 <j.alexander.lopez.f@gmail.com>
2026-05-28 04:57:23 -0500

    03-LOGIN-REGISTER

--- modulo_2/ShopApplication/app/src/main/java/com/shopapp/di/RepositoryModule.kt
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/di/RepositoryModule.kt
@@ -1,8 +1,9 @@
+// di/RepositoryModule.kt
 package com.shopapp.di
 
-// di/RepositoryModule.kt
-
+import com.shopapp.data.repository.AuthRepositoryImpl
 import com.shopapp.data.repository.CategoryRepositoryImpl
+import com.shopapp.domain.repository.AuthRepository
 import com.shopapp.domain.repository.CategoryRepository
 import dagger.Binds
 import dagger.Module
@@ -15,9 +16,8 @@ import javax.inject.Singleton
 abstract class RepositoryModule {
 
     @Binds @Singleton
-    abstract fun bindCategoryRepository(
-        impl: CategoryRepositoryImpl,
-    ): CategoryRepository
+    abstract fun bindAuthRepository(impl: AuthRepositoryImpl): AuthRepository
 
-    // Los demás repositorios se añaden en los módulos siguientes
+    @Binds @Singleton
+    abstract fun bindCategoryRepository(impl: CategoryRepositoryImpl): CategoryRepository
 }
