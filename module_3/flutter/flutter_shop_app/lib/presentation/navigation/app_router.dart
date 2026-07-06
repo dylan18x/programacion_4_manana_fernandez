d9dc4b7816d968ad1c9738137d2a28f02cbf934d
dylan18x <gadieldylan02@gmail.com>
2026-07-06 10:44:44 -0500

    fluter shop_api checkpoint 06-06-07-2026

--- module_3/flutter/flutter_shop_app/lib/presentation/navigation/app_router.dart
+++ module_3/flutter/flutter_shop_app/lib/presentation/navigation/app_router.dart
@@ -2,8 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_shop_app/presentation/screens/auth/profile_screen.dart';
 import 'package:flutter_shop_app/presentation/screens/cart/cart_screen.dart';
 import 'package:flutter_shop_app/presentation/screens/catalog/productdetailscreen.dart';
+import 'package:flutter_shop_app/presentation/screens/orders/orderdetailscreen.dart';
+import 'package:flutter_shop_app/presentation/screens/orders/orders_screen.dart';
 import 'package:go_router/go_router.dart';
 import '../../domain/model/auth_state.dart';
 import '../providers/auth_provider.dart';
@@ -97,18 +100,19 @@ final routerProvider = Provider<GoRouter>((ref) {
                 _PlaceholderScreen('Detalle #${s.pathParameters['id']} — M5'),
           ),
           GoRoute(
-              path: '/cart',
-              builder: (_, __) => const _PlaceholderScreen('Carrito — M5')),
+            path: '/orders',
+            builder: (_, __) => const OrdersScreen(),
+          ),
           GoRoute(
-              path: '/orders',
-              builder: (_, __) => const _PlaceholderScreen('Mis pedidos — M6')),
+            path: '/orders/:id',
+            builder: (_, s) => OrderDetailScreen(
+              orderId: int.parse(s.pathParameters['id']!),
+            ),
+          ),
           GoRoute(
-              path: '/orders/:id',
-              builder: (_, s) =>
-                  _PlaceholderScreen('Pedido #${s.pathParameters['id']} — M6')),
-          GoRoute(
-              path: '/profile',
-              builder: (_, __) => const _PlaceholderScreen('Perfil — M6')),
+            path: '/profile',
+            builder: (_, __) => const ProfileScreen(),
+          ),
         ],
       ),
 
