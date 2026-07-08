ad1c90d2f0fd5cfcd79e50288ded755ec11d5ef1
dylan18x <gadieldylan02@gmail.com>
2026-07-08 09:37:08 -0500

    SHOPAPP CHECKPOINT 09-ADMIN-PRODUCTS 08-07-2026

--- module_3/flutter/flutter_shop_app/lib/presentation/navigation/app_router.dart
+++ module_3/flutter/flutter_shop_app/lib/presentation/navigation/app_router.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_shop_app/presentation/screens/admin/categoriesadminscreen.dart';
 import 'package:flutter_shop_app/presentation/screens/admin/dashboard_screen.dart';
+import 'package:flutter_shop_app/presentation/screens/admin/productsadminscreen.dart';
 import 'package:flutter_shop_app/presentation/screens/auth/profile_screen.dart';
 import 'package:flutter_shop_app/presentation/screens/cart/cart_screen.dart';
 import 'package:flutter_shop_app/presentation/screens/catalog/productdetailscreen.dart';
@@ -141,7 +142,7 @@ final routerProvider = Provider<GoRouter>((ref) {
         builder: (_, state) => AdminShell(
           title: 'Productos',
           currentRoute: state.matchedLocation,
-          child: const _AdminPlaceholder('Productos — M9'),
+          child: const ProductsAdminScreen(),
         ),
       ),
       GoRoute(
